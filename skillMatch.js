0bef6fefbc6e7cbaacdd3ef82b25543759a3ad01
bbuhr <95715596+bbuhr@users.noreply.github.com>
2026-05-20 19:33:28 +0000

    feat: Criando função para trazer correspondência e habilidades faltantes

---
+++ skillMatch.js
@@ -0,0 +1,20 @@
+let tenho = ["azul","amarelo","laranja","violeta"]
+
+let precisa = ["amarelo","ciano","vermelho", "azul"]
+
+//os que faltam
+
+let falta = precisa.filter((item)=>{
+    return !tenho.includes(item)
+})
+
+let possui = precisa.filter((item)=>{
+    return tenho.includes(item)
+})
+let qtdPossui = possui.length
+let qtd = precisa.length
+
+let porcentagem = qtdPossui/qtd * 100
+
+console.log(`Faltam ${falta}`)
+console.log(`Possui ${qtdPossui} de ${qtd} itens, o que corresponde a ${porcentagem}%.`)
